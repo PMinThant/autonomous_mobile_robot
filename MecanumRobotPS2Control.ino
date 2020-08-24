095dd0b6359df8225c98754d76bded0cfb8d7aab
PMinThant <pminthant@outlook.com>
2020-08-24 11:34:06 +0630

    SPD.h

--- MecanumRobotPS2Control.ino
+++ MecanumRobotPS2Control.ino
@@ -2,7 +2,7 @@
 // 1) open Tools -> Manage Libraries...
 // 2) install "Encoder" by Paul Stoffregen v1.4.1
 #include <Encoder.h>
-
+#include "SPD.h"
 // NOTE: Requires the PS2X_lib installed.
 // 1) open Sketch -> Include Library -> Add .ZIP Library
 // 2) select "PS2X_lib.zip"
@@ -26,35 +26,9 @@ byte vibrate = 0;
 
 void (* resetFunc) (void) = 0;
 
-// --- SPD Motor ---
 
-class SPDMotor {
-  public:
-  SPDMotor( int encoderA, int encoderB, bool encoderReversed, int motorPWM, int motorDir1, int motorDir2 );
 
-  /// Set the PWM speed and direction pins.
-  /// pwm = 0, stop (no active control)
-  /// pwm = 1 to 255, proportion of CCW rotation
-  /// pwm = -1 to -255, proportion of CW rotation
-  void speed( int pwm );
 
-  /// Activate a SHORT BRAKE mode, which shorts the motor drive EM, clamping motion.
-  void hardStop();
-
-  /// Get the current speed.
-  int getSpeed();
-
-  /// Get the current rotation position from the encoder.
-  long getEncoderPosition();
-
-  private:
-    Encoder *_encoder;
-    bool _encoderReversed;
-    int _motorPWM, _motorDir1, _motorDir2;
-
-    // Current speed setting.
-    int _speed;
-};
 
 SPDMotor::SPDMotor( int encoderA, int encoderB, bool encoderReversed, int motorPWM, int motorDir1, int motorDir2 ) {
   _encoder = new Encoder(encoderA, encoderB);
